9e9f19589406794f82aadead8d13c99115806d7b
7UP-hue <2176859595@qq.com>
2022-10-20 20:18:18 +0800

    change: app.js

--- themes/light/source/js/app.js
+++ themes/light/source/js/app.js
@@ -3,25 +3,38 @@ $(function(){
    * 首页点击向下箭头页面滚动
    */
   $('.scroll-down').on('click', function(){
-    $('html,body').animate({'scrollTop':$(window).height()-80},500)
+    $('html,body').animate({'scrollTop':$(window).height()-40},500)
   })
   /**
    * 点击回到顶部
    */
   $('.scroll-top').on('click', function(){
-    $('html,body').animate({'scrollTop':$(window).height()-80},800)
+    var currentPath = window.location.pathname
+    if(currentPath === '/7UP-hue/') {
+      $('html,body').animate({'scrollTop':$(window).height()-80},800)
+    }
+    else {
+      $('html,body').animate({'scrollTop':0},800)
+    }
   })
   /**
    * 监听页面滚动
    */
   $(window).scroll(function(event){
+    var currentPath = window.location.pathname
     var pos = $(window).scrollTop()
-    if(pos >= $(window).height()-80) {
+    if(currentPath === '/7UP-hue/') {
+        if(pos >= $(window).height()-80) {
+        $('.scroll-top').show()
+        $('.header').addClass('header-last')
+      } else {
+        $('.scroll-top').hide()
+        $('.header').removeClass('header-last')
+      }
+    } 
+    else {
       $('.scroll-top').show()
       $('.header').addClass('header-last')
-    } else {
-      $('.scroll-top').hide()
-      $('.header').removeClass('header-last')
     }
   })
 })
